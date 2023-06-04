ccd2795e2d92c711595989288c17fba42805b77f
dlllonb <bassdillon10@gmail.com>
2023-06-04 13:53:29 -0400

    basic structure

--- finder_exec.py
+++ finder_exec.py
@@ -1,6 +1,28 @@
 # file to execute code from terminal
 
 import finder_code as code
-print("hi")
-code.func()
-print('bye')
+
+def help():
+    print("")
+    print("Here is the list of commands:")
+    print("  help : shows this page")
+    print("  quit : quits the program")
+    print("")
+
+def search():
+    pass
+
+
+while True:
+    command = input("c: ")
+    if command == "q" or command == "quit":
+        break
+    elif command == "help":
+        help()
+    elif command == "search":
+        search()
+    elif command == "f1":
+        code.func()
+    else:
+        print("Unknown command... type help to see list.")
+    
